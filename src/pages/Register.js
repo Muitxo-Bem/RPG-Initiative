e2cb367f6e5a1b1fe675735a6090f13d3977a962
ErikJhonatta <erikjhonatta@gmail.com>
2020-02-14 18:45:58 -0300

    Adicionando o desenvolvimento pela metade da tela Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,88 +1,92 @@
 import React, { useState } from 'react'
 import NumericInput from 'react-native-numeric-input';
-import {MaterialIcons} from '@expo/vector-icons';
-import{StyleSheet,ScrollView,View,TextInput, TouchableOpacity, Keyboard} from 'react-native';
+import{StyleSheet,KeyboardAvoidingView,Platform, Text,TouchableWithoutFeedback,Keyboard} from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import {MaterialIcons} from '@expo/vector-icons'
 
 function Register({navigation}){
+    var platform = Platform.OS;
     const [nomePlayer,setNomePlayer] = useState('');
     const [modPlayer,setModPlayer] = useState(0);
 
-    function handleRegistration(){
-        Keyboard.dismiss();
-        console.log(nomePlayer,modPlayer);
+    function dale(){
+        console.log('dale');
     }
-    return(
-        <View style={styles.container}
-                keyboardShouldPersistTaps={false}
-        >
-        <View style={styles.viewTextInput}
-            keyboardShouldPersistTaps={false}
-        >
-            
-            <TextInput
-                style={styles.textInput}
-                placeholder=" Digite o nome: "
-                placeholderTextColor= "#c4c4c4"
-                autoCapitalize= 'words'
-                autoCorrect={false}
-                keyboardAppearance='dark'
-                textContentType='username'
-                value={nomePlayer}
-                onChangeText={setNomePlayer}
-            />
-            <NumericInput 
-                type='up-down'
-                value={modPlayer}
-                onChange={setModPlayer}
-                valueType='integer'
-                textColor='#FFF'
-                minValue={-5}
-                maxValue={5}
-                containerStyle={styles.modInput}
-            />
-            <TouchableOpacity onPress={handleRegistration} style={styles.submitButton}>
-                <MaterialIcons name='add' size={40} color='#8e4dff'/>
-            </TouchableOpacity>
-        </View>
 
-            
-            
-        </View>
+    return(
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <KeyboardAvoidingView style={styles.container}>
+                <TextInput
+                    style={styles.form}
+                    placeholder='Digite um nome: '
+                    placeholderTextColor="#c9c9c9"
+                    autoCapitalize='words'
+                    autoCorrect={false}
+                    value={nomePlayer}
+                    onChangeText={setNomePlayer}
+                />
+                
+                <NumericInput
+                    containerStyle={styles.mod}
+                    type='up-down'
+                    value={modPlayer}
+                    onChange={setModPlayer}
+                    minValue={-5}
+                    maxValue={5}
+                    valueType='integer'
+                    iconSize={0}
+                    totalWidth={platform=="android" ? 119 : undefined}
+                />
+        
+                <TouchableOpacity onPress={()=>{
+                    navigation.navigate('Calc',{})
+                }} style={styles.confirmButton}>
+                    <MaterialIcons name='check' size={40} color='black'/>
+                </TouchableOpacity>
+                
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     );
 }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#151515',
-    },
-    viewTextInput:{
-        position:'absolute',
-        top:5,
-        left:15,
-        right:25,
+    container:{
         flexDirection:'row',
+        flex:1,
+        backgroundColor:'#848484',
+    },
+    form:{
+            flex: 0.5,
+            backgroundColor:'#FFF',
+            top: 5,
+            width: 180,
+            borderWidth:1,
+            left:3,
+            borderColor: 'black',
+            flexDirection: 'row',
+            paddingLeft:3,
+            fontSize:18,
+        ...Platform.select({
+            ios:{
+                height: 38,
+            },
+            android:{
+                height: 47,
+                
+            }
+        })
+    },
+    mod:{
+        flex: 0.165,
+        left: 7,
+        top:5,
         
+        backgroundColor: 'blue',
+        borderColor: 'black',
         
     },
-    textInput: {
-        flex: 1,
-        borderWidth: 1,
-        borderLeftColor:  '#FFF',
-        borderRightColor: '#FFF',
-        backgroundColor: '#FFF',
-        fontSize: 18,
-    },
-    modInput:{
-        marginLeft:10,
-        marginRight:10,
-    },
-    submitButton: {
+    confirmButton:{
         
-        borderRadius: 25,
-        backgroundColor: '#FFF',
-        
-    },
+    }
+    
 });
-
 export default Register;
